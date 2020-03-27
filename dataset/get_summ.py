355bac45477c63de9a933bc2f08edb0c043b34a9
Aditya <adit98@gmail.com>
2020-03-27 10:57:02 +0530

    Requirements and Summary extraction updated

--- dataset/get_summ.py
+++ dataset/get_summ.py
@@ -1,7 +1,17 @@
 import os
 
-read_path = '5'
-write_path = 'task_5_sum'
+read_path = 'ref_sum'
+write_path = 'gen_sum'
+
+def generate(d, author):
+	for i in sorted(os.listdir(read_path)):
+		parts = i.split('.')
+		dir_n, a = parts[0], parts[4]
+		if dir_n == d and a == author:
+			with open(read_path + '/' + i, 'r') as fd:
+				content = fd.read().strip()
+			with open(write_path + '/' + dir_n.lower() + 't/summary.txt', 'a') as fd:
+				fd.write(content + '.\n')
 
 
 for i in sorted(os.listdir(read_path)):
@@ -10,9 +20,27 @@ for i in sorted(os.listdir(read_path)):
 
 	if dir_name.lower() + 't' not in os.listdir(write_path):
 		os.mkdir(write_path + '/' + dir_name.lower() + 't')
+
+count = 0
+while True:
+	temp = []
+	all_auths = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+	for i in sorted(os.listdir(write_path)):
+		if not os.listdir(write_path + '/' + i):
+			generate(i.upper().replace('T', ''), all_auths[count])
 	
-	if author == 'A':
-		with open(read_path + '/' + i, 'r') as fd:
-			content = fd.read().strip()
-		with open(write_path + '/' + dir_name.lower() + 't/summary.txt', 'a') as fd:
-			fd.write(content + ' ')
+	for i in sorted(os.listdir(write_path)):
+		if os.listdir(write_path + '/' + i):
+			temp.append(1)
+
+	if temp.count(1) == 50:
+		break
+	else:
+		count += 1
+
+for i in sorted(os.listdir(write_path)):
+	file_path = write_path + '/' + i
+	with open(file_path + '/summary.txt', 'r') as fd:
+		content = [i for i in fd.read().split('\n') if i != '']
+		count += 1
+		print('File: {}\nSummary sentences length: {}\n'.format(file_path, len(content)))
